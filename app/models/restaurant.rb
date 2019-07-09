7450e76d2c84f6fe230d43ade4dc8677442a87ea
Rui Freitas <dinnerpartysaboteur@gmail.com>
2019-07-09 10:09:33 +0200

    rails 7 crud actions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :stars, inclusion: { in: 1..5, message: 'must be between 1 and 5' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # VERB + PATH + CONTROLLER#ACTION
+  get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
+
+  get 'restaurants/new', as: :new_restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  patch 'restaurants/:id', to: 'restaurants#update'
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
 end
